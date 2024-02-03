7e0fd435344e08f0463ea91b14049509163f28e6
raoufmaklouf <raoufmaklouf0int@gmail.com>
2024-02-03 10:04:15 +0000

    Update main.go

--- main.go
+++ main.go
@@ -24,25 +24,37 @@ func main() {
 				port, _ := strconv.Atoi(Port)
 				for x, payload := range payload_list {
 					r1 := Request1(path, host, payload, port)
+
 					if len(r1) > 1 {
-						r2 := Request2(host, port)
-						if len(r2) > 1 {
-							h2, b2, err := splitHTTPResponse(r1)
+						_, b1, err := splitHTTPResponse(r1)
+						if err == nil {
+
 							if err == nil {
 
-								scode2, err := extractStatusCode(h2)
-								if err == nil {
-									r3 := Request3(host, port)
-									if len(r3) > 1 {
-										h3, b3, err := splitHTTPResponse(r3)
-										scode3, err := extractStatusCode(h3)
+								r2 := Request2(host, port)
+								if len(r2) > 1 {
+									h2, b2, err := splitHTTPResponse(r2)
+									if err == nil {
+
+										scode2, err := extractStatusCode(h2)
 										if err == nil {
-											if b2 == b3 && scode2 == scode3 {
-												fmt.Println("\033[31m", u, "is vuln ", x, payload, "\033[0m")
+											r3 := Request3(host, port)
+											if len(r3) > 1 {
+												h3, b3, err := splitHTTPResponse(r3)
+												scode3, err := extractStatusCode(h3)
+												if err == nil {
+													if b2 == b3 && scode2 == scode3 && b1 != b2 {
+														//fmt.Println("\n======================================R1=========================================\n", r1)
+														//fmt.Println("\n======================================R2=========================================\n", r2)
+														//fmt.Println("\n======================================R3=========================================\n", r3)
+														fmt.Println("\033[31m", u, "is vuln ", x, payload, "\033[0m")
+														break
+													}
+												}
 
 											}
-										}
 
+										}
 									}
 
 								}
